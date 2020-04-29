53d4b0ed68d60bc1b6465fdbf74a17b3af01abe5
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2020-04-29 00:55:31 -0400

    actualizando la identacion en init.lua

--- init.lua
+++ init.lua
@@ -3,18 +3,13 @@
  @package   Relog
  @filename  init.lua
  @version   1.0
- @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon@openmailbox.org>
- @date      17.06.2018 01:34:35 -04
+ @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com>
+ @date      29.04.2020 01:34:35 -04
 ]]--
 
--- declaro mis variables globales
-
-require('lib.middleclass') -- la libreria middleclass me da soporte a OOP
-
-local lgi = require('lgi') -- requiero esta libreria que me permitira usar GTK
-
-local GObject = lgi.GObject -- parte de lgi
-local GLib = lgi.GLib -- para el treeview
+local lgi = require('lgi')            -- requiero esta libreria que me permitira usar GTK
+local GObject = lgi.GObject           -- parte de lgi
+local GLib = lgi.GLib                 -- para el treeview
 local Gtk = lgi.require('Gtk', '3.0') -- el objeto GTK
 
 local assert = lgi.assert
@@ -25,9 +20,9 @@ local ui = builder.objects
 
 -- declaro los objetos
 
-local main_window = ui.main_window -- invoco la ventana con el id main_window
+local main_window = ui.main_window        -- invoco la ventana con el id main_window
 local hora = builder:get_object("hora")   -- invoco al label con el id hora
-main_window:move(850,80)  --posiciona la ventana x(800),y(80)
+main_window:move(850,80)                  --posiciona la ventana x(800),y(80)
 
 -- cierro la ventana cuando se presione boton cerrar (x)
 function main_window:on_destroy()
