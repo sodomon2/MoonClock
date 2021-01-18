bd9ee6225693635ca56232ca4a3c5bf1b923bde7
Diaz Urbaneja Victor Diego Alenjandro <sodomon2@gmail.com>
2021-01-18 17:36:03 -0400

    Relog => MoonClock

---
+++ moonclock-mini.lua
@@ -0,0 +1,44 @@
+#!/usr/bin/lua
+--[[!
+ @package   MoonClock
+ @filename  moonclock-mini.lua
+ @version   1.0
+ @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon@openmailbox.org>
+ @date      17.06.2018 01:34:35 -04
+]]--
+
+local lgi   = require('lgi')
+local GLib  = lgi.GLib
+local Gtk   = lgi.require('Gtk', '3.0')
+
+local main_window = Gtk.Dialog {
+   title      = "Test Clock",
+   resizable  = false
+}
+
+main_window:set_decorated(false)
+main_window:move(900,80)
+
+local content = Gtk.Box {
+   orientation = 'VERTICAL',
+   spacing = 0,
+   border_width = 0,
+   Gtk.Label {
+      label       = os.date("%H:%M:%S"),
+      id          = "relog",
+      use_markup  = false
+   }
+}
+
+main_window:get_content_area():add(content)
+
+GLib.timeout_add_seconds(
+    GLib.PRIORITY_DEFAULT, 1,
+   function()
+        content.child.relog.label = os.date("%H:%M:%S")
+    return true
+end
+)
+
+main_window:show_all()
+Gtk.main()
---
+++ moonclock.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/lua
+--[[!
+ @package   MoonClock
+ @filename  moonclock.lua
+ @version   1.0
+ @autor     Diaz Urbaneja Victor Diego Alejandro <sodomon2@gmail.com>
+ @date      29.04.2020 01:34:35 -04
+]]--
+
+local lgi         = require('lgi')
+local GObject     = lgi.GObject
+local GLib        = lgi.GLib
+local Gtk         = lgi.require('Gtk', '3.0')
+
+local builder     = Gtk.Builder()
+local ui          = builder.objects
+builder:add_from_file('moonclock.ui')
+ui.main_window:move(850,80)
+
+ui.clock.label     = os.date("%H:%M:%S")
+GLib.timeout_add_seconds(
+    GLib.PRIORITY_DEFAULT, 1,
+   function()
+        ui.clock.label = os.date("%H:%M:%S")
+    return true
+end
+)
+
+ui.main_window:show_all()
+Gtk.main()
